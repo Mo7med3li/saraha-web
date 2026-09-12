db1bc4d1926382739fa5cce6c8c9c6a6065360ca
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-09-13 02:42:36 +0300

    feat: Refactor Input component to support additional features including password visibility toggle, addons, and error handling

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,19 +1,109 @@
-import * as React from "react"
-import { Input as InputPrimitive } from "@base-ui/react/input"
-import { cn } from "cn"
+"use client";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <InputPrimitive
-      type={type}
-      data-slot="input"
-      className={cn(
-        "h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+import * as React from "react";
 
-export { Input }
+import { cn } from "cn";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input"> & {
+    error?: boolean;
+    icon?: React.ReactNode;
+    wrapperClassName?: string;
+    heightClassName?: string;
+    addonBefore?: React.ReactNode;
+    addonAfter?: React.ReactNode;
+  }
+>(
+  (
+    {
+      className,
+      type,
+      error,
+      icon,
+      wrapperClassName,
+      heightClassName = "h-8",
+      addonBefore,
+      addonAfter,
+      ...props
+    },
+    ref,
+  ) => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const toggleShowPassword = () => setShowPassword(!showPassword);
+
+    return (
+      <div
+        className={cn(
+          "flex items-center w-full relative",
+          addonBefore || addonAfter ? "gap-0" : "gap-2",
+          wrapperClassName,
+        )}
+      >
+        {addonBefore && (
+          <div
+            className={cn(
+              "flex rounded-[10px] rounded-r-none items-center justify-center px-3 bg-muted border border-r-0 border-input rounded-l-[10px] text-muted-foreground text-sm whitespace-nowrap",
+              heightClassName,
+              error && "border-red-500 hover:border-red-500",
+            )}
+          >
+            {addonBefore}
+          </div>
+        )}
+        <input
+          type={type === "password" && showPassword ? "text" : type}
+          className={cn(
+            "flex w-full rounded-[10px] border border-input hover:border-primary focus-visible:border-primary bg-background px-3 py-1 text-base text-foreground shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:shadow-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm file:font-medium file:text-foreground file:border-0 file:bg-transparent file:text-sm",
+            heightClassName,
+            icon && !addonAfter && "pr-10",
+            addonBefore && "rounded-l-none",
+            addonAfter && "rounded-r-none",
+            className,
+            error &&
+              "border-red-500 hover:border-red-500 focus-visible:border-red-500 focus-visible:ring-0 focus-visible:shadow-none",
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <span className="absolute right-2 top-1/2 -translate-y-1/2">
+            {showPassword ? (
+              <EyeIcon
+                size={15}
+                onClick={toggleShowPassword}
+                className="cursor-pointer text-gray-500"
+              />
+            ) : (
+              <EyeOffIcon
+                size={15}
+                onClick={toggleShowPassword}
+                className="cursor-pointer text-gray-500"
+              />
+            )}
+          </span>
+        )}
+        {addonAfter && (
+          <div
+            className={cn(
+              "flex items-center rounded-[10px] rounded-l-none justify-center px-3 bg-muted border border-l-0 border-input rounded-r-[10px] text-muted-foreground text-sm whitespace-nowrap",
+              heightClassName,
+              error && "border-red-500 hover:border-red-500",
+            )}
+          >
+            {addonAfter}
+          </div>
+        )}
+        {!addonAfter && (
+          <span className="absolute right-2 top-1/2 -translate-y-1/2">
+            {icon}
+          </span>
+        )}
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
